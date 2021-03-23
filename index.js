dd74bd42ec5bbf9e1ee25b890382beffa49d381c
Graham Hall <ghall89@me.com>
2021-03-23 18:48:51 +0000

    added first couple questions and initlal markdown generation

--- index.js
+++ index.js
@@ -4,7 +4,34 @@ const inquirer = require('inquirer')
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+	{
+		type: 'input',
+		name: 'title',
+		message: "What's the project name? (required)",
+		validate: titleInput => {
+			if (titleInput) {
+				return true;
+			} else {
+				console.log('Please enter a project name!');
+				return false;
+			}
+		}
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: "Provide some information about the project. (required)",
+		validate: descriptInput => {
+			if (descriptInput) {
+				return true;
+			} else {
+				console.log('Please enter a description!');
+				return false;
+			}
+		}
+	}
+];
 
 // TODO: Create a function to write README file
 const writeToFile = data => {
@@ -18,11 +45,21 @@ const writeToFile = data => {
 
 // TODO: Create a function to initialize app
 const init = () => {
-	let readMeContent = `
-# This is just a test
-	`;
-	writeToFile(readMeContent);
-};
+	inquirer.prompt(questions)
+		.then(answers => {
+			console.log(answers);
+			let readMeContent = generateMarkdown(answers);
+			writeToFile(readMeContent);
+		})
+		.catch(error => {
+			if(error.isTtyError) {
+				console.log(error);
+			} else {
+				console.log(error);
+			}
+		});
+	};
+
 
 // Function call to initialize app
 init();
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 
-# This is just a test
-	
+# test
+
+## Description
+This is a test and I'm just writing some nonsense here to make sure it all works. Node is kinda scary but I'm learning things. Honestly, I'm shocked this worked (assuming it did!).
+
