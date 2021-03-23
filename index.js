639c3b119a2c39a5e965ee4bb2ff19afdb125da6
Graham Hall <ghall89@me.com>
2021-03-23 16:44:01 +0000

    Write test file

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = data => {
+	fs.writeFile('readme.md', data, (err) => {
+		if (err) {
+			return err;
+		}
+		console.log('Readme successfully created!');
+	});
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+	let readMeContent = `
+# This is just a test
+	`;
+	writeToFile(readMeContent);
+};
 
 // Function call to initialize app
 init();
