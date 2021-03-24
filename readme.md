1be30d7beafcab433a7eb51952a06b2129bad119
Graham Hall <ghall89@me.com>
2021-03-24 17:29:08 +0000

    added licenses

---
+++ readme.md
@@ -0,0 +1,23 @@
+
+# Readme Generator
+
+## Description
+
+A command line app that generates readme files.
+
+![](./assets/screenshot.png)
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+
+
+## Installation
+
+I'll put install instructions here
+
+## Usage
+
+And usage instructions here
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,60 +1,122 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const renderLicenseBadge = license => {}
+const renderLicenseBadge = license => {};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-const renderLicenseLink = license => {}
+const renderLicenseLink = licenseInput => {
+  if (licenseInput == 'No License') {
+    return '';
+  }
+  return `- [License](#license)`;
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-const renderLicenseSection = licenseInput => {
+const renderLicenseSection = (licenseInput, title) => {
     
   if (licenseInput == 'No License') {
     return '';
-  } else {
-    let licenseText = '';
+  }
+  let licenseText = '';
+  const currentYear = new Date().getFullYear();
     
-    if (licenseInput == 'GNU General Public License') {
-      licenseText = `
-This project is licensed under the GNU General Public License
+  if (licenseInput == 'GNU General Public License') {
+    licenseText = `
+${title}
+Copyright (C) ${currentYear}  <name of author>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
       `;
-    } else if (licenseInput == 'MIT License') {
-      licenseText = `
-This project is licensed under the MIT License
-      `;
-    } else if (licenseInput = 'Apache License') {
-      licenseText = `
-This project is licensed under the Apache License  
-      `
-    }
-    return `
+  } else if (licenseInput == 'MIT License') {
+    licenseText = `
+MIT License
+
+Copyright (c) ${currentYear} [fullname]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+    `;
+  } else if (licenseInput = 'Apache License') {
+    licenseText = `
+Copyright ${currentYear} [name of copyright owner]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+`
+  }
+  return `
 ## License
 ${licenseText}
-    `;
-  }
+  `;
 }
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   
+  const title = data.title;
   const licenseInput = data.license;
 
   
   return `
-# ${data.title}
+# ${title}
 
 ## Description
+
 ${data.description}
 
 ![](./assets/screenshot.png)
 
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(licenseInput)}
+
 ## Installation
+
 ${data.installation}
 
 ## Usage
+
 ${data.usage}
-${renderLicenseSection(licenseInput)}
+${renderLicenseSection(licenseInput, title)}
 `;
 }
 
