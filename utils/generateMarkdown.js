e366d54f58f0864d7f2d0be62403dcb68c8ec42b
Graham Hall <ghall89@me.com>
2021-03-24 18:36:25 +0000

    license badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-const renderLicenseBadge = license => {};
+const renderLicenseBadge = licenseInput => {
+  if (licenseInput == 'No License') {
+    return '';
+  }
+  return '![](https://img.shields.io/github/license/ghall89/readme-generator?style=for-the-badge)';
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -95,6 +100,7 @@ function generateMarkdown(data) {
 
   
   return `
+${renderLicenseBadge(licenseInput)}
 # ${title}
 
 ## Description
